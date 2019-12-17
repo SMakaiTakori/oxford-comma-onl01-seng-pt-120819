47ca633720830abff48b4bea46aaf96c1bd74082
S. Makai Takori <makait87@gmail.com>
2019-12-17 02:50:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.length == 1 
     "#{array[0]}"
   elsif array.length == 2
-    array.join("and")
-  elsif array.length  3
+    array.join(" and ")
+  elsif array.length >= 3
+    array.join[-1](" and ")
     
   
   # array.join(",")
