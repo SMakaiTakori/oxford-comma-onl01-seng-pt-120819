e6439e6f39ac661317cb785d46144e2a3879e79c
S. Makai Takori <makait87@gmail.com>
2019-12-17 02:47:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   if array.length == 1 
-    return "#{array[0]}"
-  elsif 
-  
-  else
+    "#{array[0]}"
+  elsif array.length == 2
+    array.join("and")
+  elsif array.length == 3
+    
   
   # array.join(",")
   end 
-end
+end 
